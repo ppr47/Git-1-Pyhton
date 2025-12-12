6233accdc93b7f6915e3ec5012d00106ded4acbd
ppr47 <piushapc@gmail.com>
2025-12-12 17:57:16 +0530

    Set 1

---
+++ Set.py
@@ -0,0 +1,12 @@
+a={"apple","banana","cherry","apple","True"}
+print(a)
+a.add("kkllm")     #add eelment
+print(a)
+
+b={1,2,3,4,5}
+a.update(b)         #mix 2 sets
+print(a)
+
+c={"a","b","c"}
+c.remove("a")
+print(c)
